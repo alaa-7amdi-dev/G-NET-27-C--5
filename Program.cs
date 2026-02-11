4f2b41711fc474d553f85de8a4556d7149922374
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-02-11 16:31:00 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -4,47 +4,77 @@
     {
         static void Main(string[] args)
         {
-        #region Q1
-        //A:
-        //    Console.WriteLine("Enter Day Number from 0 to 6");
-        //    bool isTrue = int.TryParse(Console.ReadLine(), out int day);
-        //    do
-        //    {
+            #region Q1
+            //A:
+            //    Console.WriteLine("Enter Day Number from 0 to 6");
+            //    bool isTrue = int.TryParse(Console.ReadLine(), out int day);
+            //    do
+            //    {
 
-        //        if (isTrue)
-        //        {
+            //        if (isTrue)
+            //        {
 
-        //            break;
+            //            break;
 
-        //        }
-        //        else
-        //            Console.WriteLine("Invaild");
-        //        goto A;
+            //        }
+            //        else
+            //            Console.WriteLine("Invaild");
+            //        goto A;
 
-        //    }
-        //    while (!isTrue);
+            //    }
+            //    while (!isTrue);
 
-        //    DayOfWeek dayOfWeek = (DayOfWeek)day;
+            //    DayOfWeek dayOfWeek = (DayOfWeek)day;
 
-        //    switch (dayOfWeek)
-        //    {
-        //        case DayOfWeek.Saturday:
-        //        case DayOfWeek.Sunday:
-        //        case DayOfWeek.Monday:
-        //        case DayOfWeek.Tuesday:
-        //        case DayOfWeek.Wednesday:
-        //        case DayOfWeek.Thursday:
-        //            Console.WriteLine($"Day:{Enum.GetName<DayOfWeek>(dayOfWeek)} \n It's the WorkDay");
-        //            break;
-        //        case DayOfWeek.Friday:
-        //            Console.WriteLine($"Day:{Enum.GetName<DayOfWeek>(dayOfWeek)} \n It's the Weekend");
-        //            break;
-        //        default:
-        //            Console.WriteLine("OutOfRange");
-        //            break;
+            //    switch (dayOfWeek)
+            //    {
+            //        case DayOfWeek.Saturday:
+            //        case DayOfWeek.Sunday:
+            //        case DayOfWeek.Monday:
+            //        case DayOfWeek.Tuesday:
+            //        case DayOfWeek.Wednesday:
+            //        case DayOfWeek.Thursday:
+            //            Console.WriteLine($"Day:{Enum.GetName<DayOfWeek>(dayOfWeek)} \n It's the WorkDay");
+            //            break;
+            //        case DayOfWeek.Friday:
+            //            Console.WriteLine($"Day:{Enum.GetName<DayOfWeek>(dayOfWeek)} \n It's the Weekend");
+            //            break;
+            //        default:
+            //            Console.WriteLine("OutOfRange");
+            //            break;
 
 
-        //    }
+            //    }
+            #endregion
+
+            #region Q2
+            //Console.WriteLine("Enter Size of Array : ");
+            //int.TryParse(Console.ReadLine(), out int size);
+            //int[] arr = new int[size];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.WriteLine($"\n Enter element[{i}]:");
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int sum = 0, min=arr[0], max=arr[0];
+            //double avg = 0;
+            //foreach (int item in arr)
+            //{
+            //    if (item > max)
+            //      max = item;
+            //    if (item < min)
+            //        min = item;
+            //    sum+= item;
+            //    avg =sum/size;
+            //}
+
+            //Console.WriteLine($"Sum =  {sum} \n Average ={avg} \n Max={max} \n Min=:{min}");
+            //for (int j = 0; j < size; j--)
+            //{
+            //    Console.WriteLine(arr[j]);
+            //}
+            
+
             #endregion
 
         }
