575eb64f00b0dd9f6a2c902799c1ac5c55f26830
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-02-11 16:06:12 +0200

    Q1

---
+++ Enums/DayOfWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.Enums
+{
+    internal enum DayOfWeek
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-           
+        #region Q1
+        //A:
+        //    Console.WriteLine("Enter Day Number from 0 to 6");
+        //    bool isTrue = int.TryParse(Console.ReadLine(), out int day);
+        //    do
+        //    {
+
+        //        if (isTrue)
+        //        {
+
+        //            break;
+
+        //        }
+        //        else
+        //            Console.WriteLine("Invaild");
+        //        goto A;
+
+        //    }
+        //    while (!isTrue);
+
+        //    DayOfWeek dayOfWeek = (DayOfWeek)day;
+
+        //    switch (dayOfWeek)
+        //    {
+        //        case DayOfWeek.Saturday:
+        //        case DayOfWeek.Sunday:
+        //        case DayOfWeek.Monday:
+        //        case DayOfWeek.Tuesday:
+        //        case DayOfWeek.Wednesday:
+        //        case DayOfWeek.Thursday:
+        //            Console.WriteLine($"Day:{Enum.GetName<DayOfWeek>(dayOfWeek)} \n It's the WorkDay");
+        //            break;
+        //        case DayOfWeek.Friday:
+        //            Console.WriteLine($"Day:{Enum.GetName<DayOfWeek>(dayOfWeek)} \n It's the Weekend");
+        //            break;
+        //        default:
+        //            Console.WriteLine("OutOfRange");
+        //            break;
+
+
+        //    }
+            #endregion
+
         }
     }
 }
